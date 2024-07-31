eac494b5684c1251ea3c9e780eb59cbc9d888658
Nazar832 <nazargrubij8@gmail.com>
2024-07-31 23:18:10 +0300

    Initial commit

--- client/src/common/constants/socket.constant.ts
+++ client/src/common/constants/socket.constant.ts
@@ -1,4 +1,3 @@
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3005';
-console.log(SOCKET_URL);
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export { SOCKET_URL };
